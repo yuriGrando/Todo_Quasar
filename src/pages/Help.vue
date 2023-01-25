1d903a7978c9816d44d1077d21a3af9d316eee7f
Yuri Victor Grando <yv.grando@gmail.com>
2023-01-25 14:20:52 -0300

    Subindo projeto Curso quasar e vue.js

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit justo a auctor feugiat. Fusce consequat convallis urna, quis luctus diam. Proin eu dui eu ligula fringilla faucibus. Donec vestibulum in quam ut finibus. </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur hendrerit justo a auctor feugiat. Fusce consequat convallis urna, quis luctus diam. Proin eu dui eu ligula fringilla faucibus. Donec vestibulum in quam ut finibus. </p>
+  </q-page>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
